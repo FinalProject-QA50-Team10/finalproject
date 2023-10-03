d4fea3d06059f9223bf6a05b39577f54958d597d
Daniela <danielashamatanova@gmail.com>
2023-10-03 15:57:15 +0300

    Add browsePublicPosts request

--- finalprojecttwomenandawoman/src/main/java/com/telerikacademy/testframework/api/BaseSetupMethods.java
+++ finalprojecttwomenandawoman/src/main/java/com/telerikacademy/testframework/api/BaseSetupMethods.java
@@ -1,4 +1,31 @@
 package com.telerikacademy.testframework.api;
 
+import com.telerikacademy.testframework.PropertiesManager;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.List;
+
 public class BaseSetupMethods {
+
+    private RequestSpecification getRestAssured()
+    {
+        return RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .baseUri(PropertiesManager.PropertiesManagerEnum.INSTANCE.getConfigProperties().getProperty("WEare.api.baseUrl"));
+    }
+
+    public List<Response> browsePublicPosts(){
+        return getRestAssured()
+                .when()
+                .get("/post/")
+                .thenReturn()
+                .jsonPath()
+                .get("postId");
+    }
+
 }
--- finalprojecttwomenandawoman/src/test/java/restassured/useractions/UserActionsTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/useractions/UserActionsTest.java
@@ -1,4 +1,20 @@
 package restassured.useractions;
 
-public class UserActionsTest {
+import com.telerikacademy.testframework.api.BaseSetupMethods;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+import restassured.base.BaseTestSetupBeforeAfter;
+
+import java.util.List;
+
+public class UserActionsTest extends BaseTestSetupBeforeAfter {
+    private final BaseSetupMethods userActionsAPI = new BaseSetupMethods();
+
+    private List<Response> browsePublicPostsResponse;
+
+    @Test
+    public void browseAllPublicPosts()
+    {
+        browsePublicPostsResponse = userActionsAPI.browsePublicPosts();
+    }
 }
