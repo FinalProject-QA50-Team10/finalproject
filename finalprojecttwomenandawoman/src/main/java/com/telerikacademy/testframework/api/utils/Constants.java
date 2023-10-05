7368ba2697564d26aaf9f8936420bb51fd5a06cb
Yordan Nikolov <x-normalize@protonmail.com>
2023-10-05 12:48:45 +0300

    Add some changes in Constants

--- finalprojecttwomenandawoman/src/main/java/com/telerikacademy/testframework/api/utils/Constants.java
+++ finalprojecttwomenandawoman/src/main/java/com/telerikacademy/testframework/api/utils/Constants.java
@@ -10,8 +10,20 @@ public class Constants {
     public static final String JACK_NICHOLSON_PASSWORD = "TheJack123";
     public static final String TOM_CRUISE_USERNAME = "TomCruise";
     public static final String TOM_CRUISE_PASSWORD = "HaHaHa123!";
-    public static final String POST_DESCRIPTION_INVALID = "";
-    public static final String POST_DESCRIPTION_VALID = "Initial Post by MrBeast using Postman";
+    public static final String POST_DESCRIPTION_INVALID = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit." +
+            " Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient" +
+            " montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. " +
+            "Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. " +
+            "In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. " +
+            "Nullam dictum felis eu pede mollis pretium. Integer tincidunt. " +
+            "Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus." +
+            " Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. " +
+            "Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus " +
+            "viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. " +
+            "Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. " +
+            "Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, " +
+            "sit amet adipiscing sem neque sed ipsum. Na";
+    public static final String POST_DESCRIPTION_VALID = "Valid Post";
 
     public static  final  String REGISTRATION_BODY = "{\"authorities\": [\"\"], " +
             "\"category\": " +
