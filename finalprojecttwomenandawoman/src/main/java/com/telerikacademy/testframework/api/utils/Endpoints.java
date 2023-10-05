46bfdee74c7c6def35db1342dbe5c1c4da0f7bd9
Doychin <doych_trendo@yahoo.com>
2023-10-05 11:48:32 +0300

    Commit changes to Endpoints class

--- finalprojecttwomenandawoman/src/main/java/com/telerikacademy/testframework/api/utils/Endpoints.java
+++ finalprojecttwomenandawoman/src/main/java/com/telerikacademy/testframework/api/utils/Endpoints.java
@@ -15,24 +15,24 @@ public class Endpoints {
     public static final String USERS = "/users";
     public static final String GET_USERS = "/search";
     public static final String REGISTER_USER = "/users/";
-    public static final String EDIT_USER_PROFILE = format("%s%s", BASE_API_URL, "/users/auth/%s/personal/");
+    public static final String EDIT_USER_PROFILE = "/users/auth/%s/personal/";
 
     //requests
-    public static final String SEND_REQUEST = BASE_API_URL + "/auth/request";
-    public static final String GET_USER_REQUESTS = format("%s%s", BASE_API_URL, "/auth/users/%s/request/");
-    public static final String APPROVE_REQUESTS = format("%s%s", BASE_API_URL, "/auth/users/%s/request/approve");
+    public static final String SEND_REQUEST = "/auth/request";
+    public static final String GET_USER_REQUESTS = "/auth/users/%s/request/";
+    public static final String APPROVE_REQUESTS = "/auth/users/%s/request/approve";
 
     //posts
     public static final String CREATE_POST = "/post/auth/creator";
-    public static final String EDIT_POST = BASE_API_URL + "/post/auth/editor";
-    public static final String DELETE_POST = BASE_API_URL + "/post/auth/manager";
+    public static final String EDIT_POST = "/post/auth/editor";
+    public static final String DELETE_POST = "/post/auth/manager";
     public static final String LATEST_POSTS = "/post/";
-    public static final String LIKE_POST = BASE_API_URL + "/post/auth/likesUp";
+    public static final String LIKE_POST = "/post/auth/likesUp";
 
     //comments
-    public static final String CREATE_COMMENT = BASE_API_URL + "/comment/auth/creator";
-    public static final String EDIT_COMMENT = BASE_API_URL + "/comment/auth/editor";
-    public static final String DELETE_COMMENT = BASE_API_URL + "/comment/auth/manager";
-    public static final String LIKE_COMMENT = BASE_API_URL + "/comment/auth/likesUp";
+    public static final String CREATE_COMMENT = "/comment/auth/creator";
+    public static final String EDIT_COMMENT = "/comment/auth/editor";
+    public static final String DELETE_COMMENT = "/comment/auth/manager";
+    public static final String LIKE_COMMENT = "/comment/auth/likesUp";
 
 }
