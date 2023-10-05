4dcba907d0a911a9a76b767a1f76e4f98d120386
Doychin <doych_trendo@yahoo.com>
2023-10-05 18:32:03 +0300

    Commit Delete for created post in the CommentTest

--- finalprojecttwomenandawoman/src/test/java/restassured/comments/CommentTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/comments/CommentTest.java
@@ -2,10 +2,10 @@ package restassured.comments;
 
 import com.telerikacademy.testframework.api.BaseSetupMethods;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
 import static com.telerikacademy.testframework.api.utils.Constants.*;
 
@@ -18,6 +18,7 @@ public class CommentTest {
 
     @Test
     @Order(1)
+    //FPT1-85 [Login Page] Login with valid username and valid password - JackNicholson
     public void when_userSignsIn_expected_loginSuccessful() {
         Response signInWithUserJackNicholson = posts.signInUser(JACK_NICHOLSON_USERNAME, JACK_NICHOLSON_PASSWORD);
         posts.assertStatusCode302(signInWithUserJackNicholson.statusCode());
@@ -25,6 +26,7 @@ public class CommentTest {
 
     @Test
     @Order(2)
+    //FPT1-25 [Add New Post] Generate new valid public post
     public void when_userSignsIn_expected_newPublicPostCreated() {
         Response createNewPublicPost = posts.createPublicPost(JACK_NICHOLSON_USERNAME, JACK_NICHOLSON_PASSWORD, POST_DESCRIPTION_VALID);
         posts.assertStatusCodeIsOk(createNewPublicPost.statusCode());
@@ -35,6 +37,7 @@ public class CommentTest {
 
     @Test
     @Order(3)
+    //FPT1-85 [Login Page] Login with valid username and valid password - TomCruise
     public void when_anotherUserSignsIn_expected_loginSuccessful() {
         Response signInWithUserTomCruise = posts.signInUser(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD);
         posts.assertStatusCode302(signInWithUserTomCruise.statusCode());
@@ -42,6 +45,7 @@ public class CommentTest {
 
     @Test
     @Order(4)
+    //FPT1-167 [Comment] Create Comment Successfully as Registered User
     public void when_TomCruiseCreatesComment_expected_CommentIsCreated() {
         Response signInWithUserTomCruise = posts.signInUser(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD);
         posts.assertStatusCode302(signInWithUserTomCruise.statusCode());
@@ -53,18 +57,29 @@ public class CommentTest {
 
     @Test
     @Order(5)
+    //FPT1-178 [Comment] Edit Comment Successfully as Registered User
     public void when_TomCruiseEditsComment_expected_CommentIsEdited() {
         Response editCommentResponse = posts.editComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD, EDIT_COMMENT_CONTENT, lastCommentId);
         posts.assertStatusCodeIsOk(editCommentResponse.statusCode());
-        // Add more assertions
+        //аdd more assertions
     }
 
     @Test
     @Order(6)
+    //FPT1-182 [Comment] Delete Comment Successfully as Registered User
     public void when_TomCruiseDeletesComment_expected_CommentIsDeleted() {
         Response deleteCommentResponse = posts.deleteComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD, lastCommentId);
         posts.assertStatusCodeIsOk(deleteCommentResponse.statusCode());
-        // Add more assertions here to validate the delete if needed
+        //аdd more assertions
+    }
+
+    @Test
+    @Order(7)
+    //FPT1-55 [Delete Post] Delete an Existing Public Post
+    public void when_JackNicholsonDeletesPost_expected_PostIsDeleted() {
+        Response deletePostResponse = posts.deletePublicPost(JACK_NICHOLSON_USERNAME, JACK_NICHOLSON_PASSWORD, lastPostId);
+        posts.assertStatusCodeIsOk(deletePostResponse.statusCode());
+        //аdd more assertions
     }
 
 }
