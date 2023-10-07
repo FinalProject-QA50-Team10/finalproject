a2831830d75d7fd28b9e4030abc6bfc7d62b2a70
Doychin <doych_trendo@yahoo.com>
2023-10-07 11:59:20 +0300

    Add AdminCommentEditDeleteTest class

---
+++ finalprojecttwomenandawoman/src/test/java/restassured/comments/AdminCommentEditDeleteTest.java
@@ -0,0 +1,8 @@
+package restassured.comments;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class AdminCommentEditDeleteTest {
+}
