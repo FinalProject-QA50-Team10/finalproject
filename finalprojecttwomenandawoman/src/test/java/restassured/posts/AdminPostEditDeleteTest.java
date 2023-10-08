9a18b97fe2ed8dc4680607c2a43c406cd1817788
Yordan Nikolov <x-normalize@protonmail.com>
2023-10-08 10:02:22 +0300

    Add login test for AdminPostEditDeleteTest

---
+++ finalprojecttwomenandawoman/src/test/java/restassured/posts/AdminPostEditDeleteTest.java
@@ -0,0 +1,29 @@
+package restassured.posts;
+
+import com.telerikacademy.testframework.api.BaseSetupMethods;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import static com.telerikacademy.testframework.api.utils.Constants.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class AdminPostEditDeleteTest {
+
+    private final BaseSetupMethods posts = new BaseSetupMethods();
+    private static int lastPostId;
+
+
+    @Test
+    @Order(1)
+    //FPT1-85 [Login Page] Sign in with username MrBeast
+    public void when_userSignsIn_expected_loginSuccessful() {
+        Response signInWithUserMrBeast = posts.signInUser(MR_BEAST_USERNAME, MR_BEAST_PASSWORD);
+        posts.assertStatusCode302(signInWithUserMrBeast.statusCode());
+    }
+
+
+
+}
