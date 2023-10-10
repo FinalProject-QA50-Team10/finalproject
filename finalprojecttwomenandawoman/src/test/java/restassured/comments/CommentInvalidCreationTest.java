1a6acc098135ba61397f85c3de871ca60eb3ed1c
Doychin <doych_trendo@yahoo.com>
2023-10-10 16:18:56 +0300

    Implement setup and teardown in CommentInvalidCreationTest

--- finalprojecttwomenandawoman/src/test/java/restassured/comments/CommentInvalidCreationTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/comments/CommentInvalidCreationTest.java
@@ -1,60 +1,35 @@
 package restassured.comments;
 
-import com.telerikacademy.testframework.api.ApiTestAssertions;
-import com.telerikacademy.testframework.api.BaseSetupMethods;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import restassured.base.BaseCommentTestSetupBeforeAfter;
 
 import static com.telerikacademy.testframework.api.utils.Constants.*;
 
-public class CommentInvalidCreationTest {
-
-    private final BaseSetupMethods comments = new BaseSetupMethods();
-    private final ApiTestAssertions assertions = new ApiTestAssertions();
-    private static int lastPostId;
-    private static int lastCommentId;
+public class CommentInvalidCreationTest extends BaseCommentTestSetupBeforeAfter {
 
     @BeforeEach
-    //FPT1-25 [Add New Post] Generate new valid public post
-    public void createPost() {
-
-        //FPT1-25 [Add New Post] Generate new valid public post
-        Response createNewPublicPost = comments.createPublicPost(JACK_NICHOLSON_USERNAME, JACK_NICHOLSON_PASSWORD, POST_DESCRIPTION_VALID);
-        assertions.assertStatusCode200(createNewPublicPost.statusCode());
-        assertions.assertPostContent("Valid Post");
-        assertions.assertPostIsPublic(createNewPublicPost);
-        int postId = createNewPublicPost.jsonPath().getInt("postId");
-        assertions.assertPostIdIsPositive(postId);
-        String content = createNewPublicPost.jsonPath().getString("content");
-        assertions.assertContentIsExpected(content, "Valid Post");
-        int categoryId = createNewPublicPost.jsonPath().getInt("category.id");
-        assertions.assertCategoryIdIsExpected(categoryId, 102);
-        String categoryName = createNewPublicPost.jsonPath().getString("category.name");
-        assertions.assertCategoryNameIsExpected(categoryName, "Actor");
-        lastPostId = postId;
+    public void setup() {
+        createPublicPost();
     }
 
     @AfterEach
-    //FPT1-55 [Delete Post] Delete an Existing Public Post
-    public void deleteInvalidComment() {
-        Response deletePostResponse = comments.deletePost(JACK_NICHOLSON_USERNAME, JACK_NICHOLSON_PASSWORD, lastPostId);
-        assertions.assertStatusCode200(deletePostResponse.statusCode());
-        assertions.assertResponseBodyIsEmpty(deletePostResponse);
+    public void teardown() {
+        deletePublicPost();
     }
 
     @Test
     //FPT1-170 [Comment] Create Comment With 1001 Characters as Registered User
     public void when_userCreatesInvalidComment_expect_errorStatusCode() {
-        String invalidComment = comments.generateInvalidComment();
+        String invalidComment = apiMethods.generateInvalidComment();
 
-        Response signInWithUserTomCruise = comments.signInUser(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD);
+        Response signInWithUserTomCruise = apiMethods.signInUser(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD);
         assertions.assertStatusCode302(signInWithUserTomCruise.statusCode());
 
-        Response createCommentResponse = comments.createComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD, invalidComment, lastPostId);
+        Response createCommentResponse = apiMethods.createComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD,
+                invalidComment, lastPostId);
         assertions.assertStatusCode400(createCommentResponse.statusCode());
     }
-
 }
