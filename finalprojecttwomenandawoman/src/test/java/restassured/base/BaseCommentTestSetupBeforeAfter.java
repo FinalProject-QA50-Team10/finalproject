66c83bd5bec413056bff61e7473629850fabc744
Doychin <doych_trendo@yahoo.com>
2023-10-10 15:46:38 +0300

    Implement setup and teardown in CommentManipulationTest

--- finalprojecttwomenandawoman/src/test/java/restassured/base/BaseCommentTestSetupBeforeAfter.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/base/BaseCommentTestSetupBeforeAfter.java
@@ -1,4 +1,30 @@
 package restassured.base;
 
+import io.restassured.response.Response;
+
+import static com.telerikacademy.testframework.api.utils.Constants.*;
+
 public class BaseCommentTestSetupBeforeAfter extends BasePostTestSetupBeforeAfter {
+
+    protected static int lastCommentId;
+
+    public void createComment() {
+        //FPT1-167 [Comment] Create Comment Successfully as Registered User
+        Response signInWithUserTomCruise = apiMethods.signInUser(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD);
+        assertions.assertStatusCode302(signInWithUserTomCruise.statusCode());
+
+        Response createCommentResponse = apiMethods.createComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD,
+                COMMENT_DESCRIPTION_VALID, lastPostId);
+        assertions.assertStatusCode200(createCommentResponse.statusCode());
+
+        lastCommentId = createCommentResponse.jsonPath().getInt("commentId");
+    }
+
+    public void deleteComment() {
+        //FPT1-182 [Comment] Delete Comment Successfully as Registered User
+        Response deleteCommentResponse = apiMethods.deleteComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD,
+                lastCommentId);
+        assertions.assertStatusCode200(deleteCommentResponse.statusCode());
+        assertions.assertResponseBodyIsEmpty(deleteCommentResponse);
+    }
 }
--- finalprojecttwomenandawoman/src/test/java/restassured/comments/CommentManipulationTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/comments/CommentManipulationTest.java
@@ -1,71 +1,32 @@
 package restassured.comments;
 
-import com.telerikacademy.testframework.api.ApiTestAssertions;
-import com.telerikacademy.testframework.api.BaseSetupMethods;
-import com.telerikacademy.testframework.api.models.CommentModel;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import restassured.base.BaseCommentTestSetupBeforeAfter;
 
 import static com.telerikacademy.testframework.api.utils.Constants.*;
 
-public class CommentManipulationTest {
-
-    private final BaseSetupMethods comments = new BaseSetupMethods();
-    private final ApiTestAssertions assertions = new ApiTestAssertions();
-    private static int lastPostId;
-    private static int lastCommentId;
+public class CommentManipulationTest extends BaseCommentTestSetupBeforeAfter {
 
     @BeforeEach
-    //FPT1-25 [Add New Post] Generate new valid public post
-    //FPT1-167 [Comment] Create Comment Successfully as Registered User
-    public void createPostAndComment() {
-
-        //FPT1-25 [Add New Post] Generate new valid public post
-        Response createNewPublicPost = comments.createPublicPost(JACK_NICHOLSON_USERNAME, JACK_NICHOLSON_PASSWORD, POST_DESCRIPTION_VALID);
-        assertions.assertStatusCode200(createNewPublicPost.statusCode());
-        assertions.assertPostContent("Valid Post");
-        assertions.assertPostIsPublic(createNewPublicPost);
-        int postId = createNewPublicPost.jsonPath().getInt("postId");
-        assertions.assertPostIdIsPositive(postId);
-        String content = createNewPublicPost.jsonPath().getString("content");
-        assertions.assertContentIsExpected(content, "Valid Post");
-        int categoryId = createNewPublicPost.jsonPath().getInt("category.id");
-        assertions.assertCategoryIdIsExpected(categoryId, 102);
-        String categoryName = createNewPublicPost.jsonPath().getString("category.name");
-        assertions.assertCategoryNameIsExpected(categoryName, "Actor");
-        lastPostId = postId;
-
-        //FPT1-167 [Comment] Create Comment Successfully as Registered User
-        Response signInWithUserTomCruise = comments.signInUser(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD);
-        assertions.assertStatusCode302(signInWithUserTomCruise.statusCode());
-        Response createCommentResponse = comments.createComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD, COMMENT_DESCRIPTION_VALID, lastPostId);
-        assertions.assertStatusCode200(createCommentResponse.statusCode());
-
-        var t = createCommentResponse.as(CommentModel.class);
-        String commentContent = t.content;
-        assertions.assertContentIsExpected(commentContent, "Valid Comment");
-        lastCommentId = createCommentResponse.jsonPath().getInt("commentId");
+    public void setup() {
+        createPublicPost();
+        createComment();
     }
 
     @AfterEach
-    //FPT1-55 [Delete Post] Delete an Existing Public Post
-    //FPT1-182 [Comment] Delete Comment Successfully as Registered User
-    public void deletePostAndComment() {
-        Response deleteCommentResponse = comments.deleteComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD, lastCommentId);
-        assertions.assertStatusCode200(deleteCommentResponse.statusCode());
-        assertions.assertResponseBodyIsEmpty(deleteCommentResponse);
-        Response deletePostResponse = comments.deletePost(JACK_NICHOLSON_USERNAME, JACK_NICHOLSON_PASSWORD, lastPostId);
-        assertions.assertStatusCode200(deletePostResponse.statusCode());
-        assertions.assertResponseBodyIsEmpty(deletePostResponse);
+    public void teardown() {
+        deleteComment();
+        deletePublicPost();
     }
 
     @Test
     //FPT1-178 [Comment] Edit Comment Successfully as Registered User
     public void when_userEditsComment_expect_commentIsEdited() {
-        Response editCommentResponse = comments.editComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD, EDIT_COMMENT_CONTENT, lastCommentId);
+        Response editCommentResponse = apiMethods.editComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD, EDIT_COMMENT_CONTENT,
+                lastCommentId);
         assertions.assertStatusCode200(editCommentResponse.statusCode());
     }
-
 }
