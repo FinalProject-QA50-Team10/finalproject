b6696ed262d1643aa0972f983216b8e85fbac02a
Doychin <doych_trendo@yahoo.com>
2023-10-10 13:59:15 +0300

    Add comments for test cases and assertions in PublicPostLikeTest

--- finalprojecttwomenandawoman/src/test/java/restassured/posts/PublicPostLikeTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/posts/PublicPostLikeTest.java
@@ -26,16 +26,26 @@ public class PublicPostLikeTest extends BaseTestSetupBeforeAfter {
 
         //FPT1-85 [Login Page] Login with valid username and valid password
         Response signInWithUserGeorgeBush = posts.signInUser(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD);
+
+        // Assert that the HTTP status code is 302 (Found/Redirect)
         assertions.assertStatusCode302(signInWithUserGeorgeBush.statusCode());
 
-        //FPT1-123 [Like] Verify posts Like button
+        // Like a public post
         Response likePublicPost = posts.likePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, lastPostId);
+
+        // Assert that the HTTP status code is 200 (OK)
         assertions.assertStatusCode200(likePublicPost.statusCode());
+
+        // Assert that the post is liked
         assertions.assertPostIsLiked(likePublicPost);
 
-        //FTP1-124 [Like] Verify posts Dislike button
+        // Dislike the same public post
         Response dislikePublicPost = posts.dislikePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, lastPostId);
+
+        // Assert that the HTTP status code is 200 (OK)
         assertions.assertStatusCode200(dislikePublicPost.statusCode());
+
+        // Assert that the post is disliked
         assertions.assertPostIsDisliked(dislikePublicPost);
     }
 
@@ -43,6 +53,8 @@ public class PublicPostLikeTest extends BaseTestSetupBeforeAfter {
     //FPT1-55 [Delete Post] Delete an Existing Public Post
     public void when_userDeletePost_expect_lastPublicPostDeleted() {
         Response lastPublicPostDeleted = posts.getLastPost(lastPostId);
+
+        // Assert that the HTTP status code is 404 (Not Found)
         assertions.assertStatusCode404(lastPublicPostDeleted.statusCode());
     }
 }
