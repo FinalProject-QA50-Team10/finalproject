f269944874b6da5f6886a9d78911c4028d4fc921
Doychin <doych_trendo@yahoo.com>
2023-10-10 13:54:18 +0300

    Add comments for test cases and assertions in PublicPostLikeTest

--- finalprojecttwomenandawoman/src/test/java/restassured/posts/PublicPostLikeTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/posts/PublicPostLikeTest.java
@@ -22,20 +22,25 @@ public class PublicPostLikeTest extends BaseTestSetupBeforeAfter {
     }
 
     @Test
-    //FPT1-123 [Like] Like a public post of another user
-    //FTP1-124 [Like] Dislike a public post of another user
     public void when_userLikeAndDislikePublicPost_expected_likeAndDislikeAnotherUserPublicPost() {
+
+        //FPT1-85 [Login Page] Login with valid username and valid password
         Response signInWithUserGeorgeBush = posts.signInUser(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD);
         assertions.assertStatusCode302(signInWithUserGeorgeBush.statusCode());
-        Response likePublicPost = posts.likePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, lastPostId);  // Use lastPostId from base class
+
+        //FPT1-123 [Like] Verify posts Like button
+        Response likePublicPost = posts.likePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, lastPostId);
         assertions.assertStatusCode200(likePublicPost.statusCode());
         assertions.assertPostIsLiked(likePublicPost);
-        Response dislikePublicPost = posts.dislikePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, lastPostId);  // Use lastPostId from base class
+
+        //FTP1-124 [Like] Verify posts Dislike button
+        Response dislikePublicPost = posts.dislikePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, lastPostId);
         assertions.assertStatusCode200(dislikePublicPost.statusCode());
         assertions.assertPostIsDisliked(dislikePublicPost);
     }
 
     @Test
+    //FPT1-55 [Delete Post] Delete an Existing Public Post
     public void when_userDeletePost_expect_lastPublicPostDeleted() {
         Response lastPublicPostDeleted = posts.getLastPost(lastPostId);
         assertions.assertStatusCode404(lastPublicPostDeleted.statusCode());
--- finalprojecttwomenandawoman/src/test/java/restassured/posts/PublicPostTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/posts/PublicPostTest.java
@@ -23,28 +23,44 @@ public class PublicPostTest extends BaseTestSetupBeforeAfter {
 
     @Test
     //FTP1-35 [Add New Post] Generate new invalid public post
-    //Check it for before each
     public void when_userCreateInvalidPublicPost_expect_invalidPublicPostNotBeCreated() {
-        Response createNewInvalidPublicPost = posts.createPublicPost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD, POST_DESCRIPTION_INVALID);
+        Response createNewInvalidPublicPost = posts.createPublicPost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD,
+                POST_DESCRIPTION_INVALID);
         var registrationErrorModel = createNewInvalidPublicPost.as(ErrorModel.class);
+
+        // Assert that the HTTP status code is 400 (Bad Request)
         assertions.assertStatusCode400(createNewInvalidPublicPost.statusCode());
+
+        // Assert that the error model indicates a bad request
         assertions.assertBadRequest(registrationErrorModel);
+
+        // Assert that the post content is invalid
         assertions.assertInvalidPostContent(createNewInvalidPublicPost);
     }
 
     @Test
     //FTP1-45 [Edit Post] Edit existing public post
     public void when_userEditPublicPost_expect_postIsEdited() {
+
+        //FPT1-85 [Login Page] Login with valid username and valid password
         Response signInResponse = posts.signInUser(MR_BEAST_USERNAME, MR_BEAST_PASSWORD);
+
+        // Assert that the HTTP status code is 302 (Found/Redirect)
         assertions.assertStatusCode302(signInResponse.statusCode());
-        Response editPublicPost = posts.editPublicPost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD, EDIT_POST_DESCRIPTION_VALID, lastPostId);
+
+        Response editPublicPost = posts.editPublicPost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD,
+                EDIT_POST_DESCRIPTION_VALID, lastPostId);
+
+        // Assert that the HTTP status code is 200 (OK)
         assertions.assertStatusCode200(editPublicPost.statusCode());
     }
 
     @Test
+    //FPT1-55 [Delete Post] Delete an Existing Public Post
     public void when_userDeletePost_expect_lastPublicPostDeleted() {
         Response lastPublicPostDeleted = posts.getLastPost(lastPostId);
+
+        // Assert that the HTTP status code is 404 (Not Found)
         assertions.assertStatusCode404(lastPublicPostDeleted.statusCode());
     }
-
 }
