a41e2139014a2c0bd2a41533a62e6ce7b1a22c6e
Doychin <doych_trendo@yahoo.com>
2023-10-10 13:04:28 +0300

    Implement setup and teardown in PublicPostLikeTest

--- finalprojecttwomenandawoman/src/test/java/restassured/posts/PublicPostLikeTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/posts/PublicPostLikeTest.java
@@ -1,38 +1,24 @@
 package restassured.posts;
 
-import com.telerikacademy.testframework.api.ApiTestAssertions;
-import com.telerikacademy.testframework.api.BaseSetupMethods;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import restassured.base.BaseTestSetupBeforeAfter;
 
-import static com.telerikacademy.testframework.api.utils.Constants.*;
+import static com.telerikacademy.testframework.api.utils.Constants.GEORGE_BUSH_PASSWORD;
+import static com.telerikacademy.testframework.api.utils.Constants.GEORGE_BUSH_USERNAME;
 
-public class PublicPostLikeTest {
-
-    private final BaseSetupMethods posts = new BaseSetupMethods();
-    private final ApiTestAssertions assertions = new ApiTestAssertions();
-    private static int lastPostId;
+public class PublicPostLikeTest extends BaseTestSetupBeforeAfter {
 
     @BeforeEach
-    //FPT1-25 [Add New Post] Generate new valid public post
-    public void createPost() {
-        Response createNewPublicPost = posts.createPublicPost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD, POST_DESCRIPTION_VALID);
-        assertions.assertStatusCode200(createNewPublicPost.statusCode());
-        assertions.assertPostContent("Valid Post");
-        assertions.assertPostIsPublic(createNewPublicPost);
-        int postId = createNewPublicPost.jsonPath().getInt("postId");
-        assertions.assertPostIdIsPositive(postId);
-        lastPostId = createNewPublicPost.jsonPath().getInt("postId");
+    public void setup() {
+        createPublicPost();
     }
 
     @AfterEach
-    //FPT1-55 [Delete Post] Delete the latest public post
-    public void deletePost() {
-        Response signInResponse = posts.signInUser(MR_BEAST_USERNAME, MR_BEAST_PASSWORD);
-        assertions.assertStatusCode302(signInResponse.statusCode());
-        Response deletePublicPost = posts.deletePost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD, lastPostId);
-        assertions.assertStatusCode200(deletePublicPost.statusCode());
-        assertions.assertResponseBodyIsEmpty(deletePublicPost);
+    public void teardown() {
+        deletePublicPost();
     }
 
     @Test
@@ -41,10 +27,10 @@ public class PublicPostLikeTest {
     public void when_userLikeAndDislikePublicPost_expected_likeAndDislikeAnotherUserPublicPost() {
         Response signInWithUserGeorgeBush = posts.signInUser(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD);
         assertions.assertStatusCode302(signInWithUserGeorgeBush.statusCode());
-        Response likePublicPost = posts.likePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, lastPostId);
+        Response likePublicPost = posts.likePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, lastPostId);  // Use lastPostId from base class
         assertions.assertStatusCode200(likePublicPost.statusCode());
         assertions.assertPostIsLiked(likePublicPost);
-        Response dislikePublicPost = posts.dislikePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, lastPostId);
+        Response dislikePublicPost = posts.dislikePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, lastPostId);  // Use lastPostId from base class
         assertions.assertStatusCode200(dislikePublicPost.statusCode());
         assertions.assertPostIsDisliked(dislikePublicPost);
     }
