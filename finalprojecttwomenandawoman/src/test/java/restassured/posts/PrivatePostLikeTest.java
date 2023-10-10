30c9fd5b08e8acf908d8241da9bd39c0b3a31f2d
Doychin <doych_trendo@yahoo.com>
2023-10-10 13:21:45 +0300

    Implement setup and teardown in PrivatePostLikeTest

--- finalprojecttwomenandawoman/src/test/java/restassured/posts/PrivatePostLikeTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/posts/PrivatePostLikeTest.java
@@ -1,46 +1,32 @@
 package restassured.posts;
 
-import com.telerikacademy.testframework.api.ApiTestAssertions;
-import com.telerikacademy.testframework.api.BaseSetupMethods;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import restassured.base.BaseTestSetupBeforeAfter;
 
-import static com.telerikacademy.testframework.api.utils.Constants.*;
+import static com.telerikacademy.testframework.api.utils.Constants.MR_BEAST_PASSWORD;
+import static com.telerikacademy.testframework.api.utils.Constants.MR_BEAST_USERNAME;
 
-public class PrivatePostLikeTest {
-
-    private final BaseSetupMethods posts = new BaseSetupMethods();
-    private final ApiTestAssertions assertions = new ApiTestAssertions();
-    private static int lastPostId;
+public class PrivatePostLikeTest extends BaseTestSetupBeforeAfter {
 
     @BeforeEach
-    //FPT1-26 [Add New Post] Generate new valid private post
-    public void createPost() {
-        Response createNewPrivatePost = posts.createPrivatePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, POST_DESCRIPTION_VALID);
-        assertions.assertStatusCode200(createNewPrivatePost.statusCode());
-        assertions.assertPostContent("Valid Post");
-        assertions.assertPostIsNotPublic(createNewPrivatePost);
-        int postId = createNewPrivatePost.jsonPath().getInt("postId");
-        assertions.assertPostIdIsPositive(postId);
-        lastPostId = createNewPrivatePost.jsonPath().getInt("postId");
+    public void setup() {
+        createPrivatePost();
     }
 
     @AfterEach
-    //FPT1-56 [Delete Post] Delete the latest private post
-    public void deletePost() {
-        Response signInResponse = posts.signInUser(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD);
-        assertions.assertStatusCode302(signInResponse.statusCode());
-        Response deletePrivatePost = posts.deletePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, lastPostId);
-        assertions.assertStatusCode200(deletePrivatePost.statusCode());
-        assertions.assertResponseBodyIsEmpty(deletePrivatePost);
+    public void teardown() {
+        deletePrivatePost();
     }
 
     @Test
     //FPT1-123 [Like] Like a private post of another user
     //FTP1-124 [Like] Dislike a private post of another user
     public void when_userLikePrivatePost_expected_likeAnotherUserPrivatePost() {
-        Response signInWithUserGeorgeBush = posts.signInUser(MR_BEAST_USERNAME, MR_BEAST_PASSWORD);
-        assertions.assertStatusCode302(signInWithUserGeorgeBush.statusCode());
+        Response signInWithUserMrBeast = posts.signInUser(MR_BEAST_USERNAME, MR_BEAST_PASSWORD);
+        assertions.assertStatusCode302(signInWithUserMrBeast.statusCode());
         Response likePrivatePost = posts.likePost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD, lastPostId);
         assertions.assertStatusCode200(likePrivatePost.statusCode());
         assertions.assertPostIsLiked(likePrivatePost);
@@ -54,5 +40,4 @@ public class PrivatePostLikeTest {
         Response lastPrivatePostDeleted = posts.getLastPost(lastPostId);
         assertions.assertStatusCode404(lastPrivatePostDeleted.statusCode());
     }
-
 }
