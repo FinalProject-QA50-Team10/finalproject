bacd4c0f6ad0bde7064b6a25c2d96564a6ad3b03
Doychin <doych_trendo@yahoo.com>
2023-10-10 14:07:42 +0300

    Add comments for test cases and assertions in PrivatePostTest

--- finalprojecttwomenandawoman/src/test/java/restassured/posts/PrivatePostTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/posts/PrivatePostTest.java
@@ -24,25 +24,44 @@ public class PrivatePostTest extends BaseTestSetupBeforeAfter {
     @Test
     //FTP1-36 [Add New Post] Generate new invalid private post
     public void when_userCreateInvalidPrivatePost_expect_invalidPrivatePostNotBeCreated() {
-        Response createNewInvalidPrivatePost = posts.createPrivatePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, POST_DESCRIPTION_INVALID);
-        var registrationErrorModel = createNewInvalidPrivatePost.as(ErrorModel.class);
+        Response createNewInvalidPrivatePost = posts.createPrivatePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD,
+                POST_DESCRIPTION_INVALID);
+
+        // Assert that the HTTP status code is 400 (Bad Request)
         assertions.assertStatusCode400(createNewInvalidPrivatePost.statusCode());
+
+        // Assert that the body contains the expected error model
+        var registrationErrorModel = createNewInvalidPrivatePost.as(ErrorModel.class);
         assertions.assertBadRequest(registrationErrorModel);
+
+        // Assert that the post content is invalid
         assertions.assertInvalidPostContent(createNewInvalidPrivatePost);
     }
 
     @Test
     //FTP1-46 [Edit Post] Edit existing private post
     public void when_userEditPrivatePost_expect_postIsEdited() {
+
+        //FPT1-85 [Login Page] Login with valid username and valid password
         Response signInResponse = posts.signInUser(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD);
+
+        // Assert that the HTTP status code is 302 (Found/Redirect)
         assertions.assertStatusCode302(signInResponse.statusCode());
-        Response editPrivatePost = posts.editPrivatePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, EDIT_POST_DESCRIPTION_VALID, lastPostId);
+
+        // Edit the existing private post
+        Response editPrivatePost = posts.editPrivatePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD,
+                EDIT_POST_DESCRIPTION_VALID, lastPostId);
+
+        // Assert that the HTTP status code is 200 (OK)
         assertions.assertStatusCode200(editPrivatePost.statusCode());
     }
 
     @Test
+    //FPT1-56 [Delete Post] Delete an Existing Private Post
     public void when_userDeletePost_expect_lastPrivatePostDeleted() {
         Response lastPrivatePostDeleted = posts.getLastPost(lastPostId);
+
+        // Assert that the HTTP status code is 404 (Not Found)
         assertions.assertStatusCode404(lastPrivatePostDeleted.statusCode());
     }
 }
