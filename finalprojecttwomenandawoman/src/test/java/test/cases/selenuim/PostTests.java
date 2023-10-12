c0112405c7d08c2bf33a887b3d75a1774c5f72e6
Yordan Nikolov <x-normalize@protonmail.com>
2023-10-12 11:09:52 +0300

    Add login and logout in PostTests

--- finalprojecttwomenandawoman/src/test/java/test/cases/selenuim/PostTests.java
+++ finalprojecttwomenandawoman/src/test/java/test/cases/selenuim/PostTests.java
@@ -1,15 +1,35 @@
 package test.cases.selenuim;
 
-import com.telerikacademy.testframework.pages.AdminHomePage;
 import com.telerikacademy.testframework.pages.LoginPage;
 import com.telerikacademy.testframework.pages.PostPage;
-import com.telerikacademy.testframework.pages.UnauthenticatedUserHomePage;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static com.telerikacademy.testframework.pages.Constants.*;
 
 public class PostTests extends BaseTestSetup {
 
     PostPage post = new PostPage(actions.getDriver());
-    LoginPage login = new LoginPage(actions.getDriver());
 
+    @BeforeEach
+    public void login() {
+        post.navigateToPage();
+        post.assertPageNavigated();
+        LoginPage.login(MR_BEAST_USERNAME, MR_BEAST_PASSWORD);
+        actions.assertElementPresent(LOGOUT_BUTTON_PATH);
+        actions.assertElementPresent(PERSONAL_PROFILE_BUTTON_PATH);
+        actions.assertElementPresent(ADMIN_ZONE_BUTTON);
+    }
+
+    @AfterEach
+    public void logOut() {
+        actions.waitForElementClickable(HOME_BUTTON_PATH);
+        actions.clickElement(HOME_BUTTON_PATH);
+        actions.waitForElementClickable(LOG_OUT_BUTTON_PATH);
+        actions.clickElement(LOG_OUT_BUTTON_PATH);
+        actions.assertElementPresent(LOGIN_PAGE_TITLE_PATH);
+        actions.assertElementPresent(LOGOUT_ERROR_MESSAGE_PATH);
+    }
 
 
 }
