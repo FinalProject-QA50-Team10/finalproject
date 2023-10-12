34dfe6a2e4249662159da281c308631e954c58f6
Yordan Nikolov <x-normalize@protonmail.com>
2023-10-12 10:47:30 +0300

    Add PostTest and update AdminTests

--- finalprojecttwomenandawoman/src/main/java/com/telerikacademy/testframework/pages/PostPage.java
+++ finalprojecttwomenandawoman/src/main/java/com/telerikacademy/testframework/pages/PostPage.java
@@ -4,10 +4,12 @@ import org.openqa.selenium.WebDriver;
 
 import static com.telerikacademy.testframework.pages.Constants.HOME_PAGE;
 
-public class PostPage extends BasePage{
-    public PostPage(WebDriver driver, String urlKey) {
+public class PostPage extends BasePage {
+    public PostPage(WebDriver driver) {
         super(driver, HOME_PAGE);
     }
 
+    public void createPost() {
 
+    }
 }
---
+++ finalprojecttwomenandawoman/src/test/java/test/cases/selenuim/PostTests.java
@@ -0,0 +1,15 @@
+package test.cases.selenuim;
+
+import com.telerikacademy.testframework.pages.AdminHomePage;
+import com.telerikacademy.testframework.pages.LoginPage;
+import com.telerikacademy.testframework.pages.PostPage;
+import com.telerikacademy.testframework.pages.UnauthenticatedUserHomePage;
+
+public class PostTests extends BaseTestSetup {
+
+    PostPage post = new PostPage(actions.getDriver());
+    LoginPage login = new LoginPage(actions.getDriver());
+
+
+
+}
