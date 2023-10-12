6f63ea0e0295e20c89cae4278d22ea5cb281c18e
Doychin <doych_trendo@yahoo.com>
2023-10-12 10:56:19 +0300

    Add Constructor to RegistrationPage

--- finalprojecttwomenandawoman/src/main/java/com/telerikacademy/testframework/pages/RegistrationPage.java
+++ finalprojecttwomenandawoman/src/main/java/com/telerikacademy/testframework/pages/RegistrationPage.java
@@ -1,4 +1,11 @@
 package com.telerikacademy.testframework.pages;
 
-public class RegistrationPage {
+import org.openqa.selenium.WebDriver;
+
+import static com.telerikacademy.testframework.pages.Constants.HOME_PAGE;
+
+public class RegistrationPage extends BasePage {
+    public RegistrationPage(WebDriver driver) {
+        super(driver, HOME_PAGE);
+    }
 }
