999a6d768330f2439aad9ff566f0fde394897627
Doychin <doych_trendo@yahoo.com>
2023-10-16 12:15:51 +0300

    Formatting changes to admin page and test for selenium

--- finalprojecttwomenandawoman/src/test/java/test/cases/selenuim/AdminTests.java
+++ finalprojecttwomenandawoman/src/test/java/test/cases/selenuim/AdminTests.java
@@ -21,7 +21,7 @@ public class AdminTests extends BaseTestSetup {
     @Test
     //FPT1-58 [See Profile] Disable User Profile as Admin
     //FPT1-59 [See Profile] Enable User Profile as Admin
-    public void when_AdminDisableAndEnableUsers_expected_UserIsDisableAndEnable() {
+    public void when_AdminDisablesAndEnablesUser_expect_UserIsDisabledAndEnabled() {
         postPage.assertPageNavigated();
         loginPage.login(ADMIN_NAME, ADMIN_PASSWORD);
         adminHomePage.disableUserWithAdminAccess();
@@ -49,7 +49,7 @@ public class AdminTests extends BaseTestSetup {
 
     @Test
     //FTP1-61 [Delete Post] Delete an Existing Post as Admin
-    public void when_AdminDeletePost_expect_PostIsDeleted() {
+    public void when_AdminDeletesPost_expect_PostIsDeleted() {
         postPage.assertPageNavigated();
         loginPage.login(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD);
         postPage.createPost("public");
@@ -64,7 +64,7 @@ public class AdminTests extends BaseTestSetup {
 
     @Test
     //FPT1-62 [Edit Comment] Edit an Existing Comment as Admin
-    public void when_AdminEditComment_expect_CommentIsEdited() {
+    public void when_AdminEditsComment_expect_CommentIsEdited() {
         postPage.assertPageNavigated();
         loginPage.login(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD);
         postPage.createPost("public");
@@ -86,7 +86,7 @@ public class AdminTests extends BaseTestSetup {
 
     @Test
     //FPT1-63 [Delete Comment] Delete an Existing Comment as Admin
-    public void when_AdminDeleteComment_expect_CommentIsDeleted() {
+    public void when_AdminDeletesComment_expect_CommentIsDeleted() {
         loginPage.assertPageNavigated();
         loginPage.login(MR_BEAST_USERNAME, MR_BEAST_PASSWORD);
         postPage.createPost("public");
