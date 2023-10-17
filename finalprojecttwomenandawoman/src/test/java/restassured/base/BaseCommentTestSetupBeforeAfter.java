a9cf2d4ad88f910eb0bb6852120ce02ff6969b33
Doychin <doych_trendo@yahoo.com>
2023-10-17 12:19:44 +0300

    Remove unused methods from Base classes

--- finalprojecttwomenandawoman/src/test/java/restassured/base/BaseCommentTestSetupBeforeAfter.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/base/BaseCommentTestSetupBeforeAfter.java
@@ -1,32 +1,6 @@
 package restassured.base;
 
-import io.restassured.response.Response;
-
-import static com.telerikacademy.testframework.api.utils.Constants.*;
-
 public class BaseCommentTestSetupBeforeAfter extends BasePostTestSetupBeforeAfter {
-
     protected static int lastCommentId;
 
-    public void createCommentAsRegisteredUser() {
-        //FPT1-167 [Comment] Create Comment Successfully as Registered User
-        Response signInWithUserTomCruise = apiMethods.signInUser(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD);
-
-        assertions.assertStatusCode302(signInWithUserTomCruise.statusCode());
-
-        Response createCommentResponse = apiMethods.createComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD,
-                COMMENT_DESCRIPTION_VALID, lastPostId);
-
-        assertions.assertStatusCode200(createCommentResponse.statusCode());
-        lastCommentId = createCommentResponse.jsonPath().getInt("commentId");
-    }
-
-    public void deleteCommentAsRegisteredUser() {
-        //FPT1-182 [Comment] Delete Comment Successfully as Registered User
-        Response deleteCommentResponse = apiMethods.deleteComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD,
-                lastCommentId);
-
-        assertions.assertStatusCode200(deleteCommentResponse.statusCode());
-        assertions.assertResponseBodyIsEmpty(deleteCommentResponse);
-    }
 }
--- finalprojecttwomenandawoman/src/test/java/restassured/base/BasePostTestSetupBeforeAfter.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/base/BasePostTestSetupBeforeAfter.java
@@ -2,71 +2,11 @@ package restassured.base;
 
 import com.telerikacademy.testframework.api.ApiTestAssertions;
 import com.telerikacademy.testframework.api.BaseSetupMethods;
-import io.restassured.response.Response;
-
-import static com.telerikacademy.testframework.api.utils.Constants.*;
 
 public class BasePostTestSetupBeforeAfter {
-
     protected final BaseSetupMethods apiMethods = new BaseSetupMethods();
+
     protected final ApiTestAssertions assertions = new ApiTestAssertions();
     protected static int lastPostId;
 
-    public void when_UserCreatesValidPublicPost_expect_ValidPublicPostIsCreated() {
-        //FPT1-25 [Add New Post] Create Public Post
-        Response createNewPublicPost = apiMethods.createPublicPost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD,
-                POST_DESCRIPTION_VALID);
-
-        assertions.assertStatusCode200(createNewPublicPost.statusCode());
-        assertions.assertPostContent("Valid Post");
-        assertions.assertPostIsPublic(createNewPublicPost);
-
-        int postId = createNewPublicPost.jsonPath().getInt("postId");
-
-        assertions.assertPostIdIsPositive(postId);
-
-        lastPostId = createNewPublicPost.jsonPath().getInt("postId");
-    }
-
-    public void when_UserCreatesValidPrivatePost_expect_ValidPrivatePostIsCreated() {
-        //FPT1-26 [Add New Post] Create Private Post
-        Response createNewPrivatePost = apiMethods.createPrivatePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD,
-                POST_DESCRIPTION_VALID);
-
-        assertions.assertStatusCode200(createNewPrivatePost.statusCode());
-        assertions.assertPostContent("Valid Post");
-        assertions.assertPostIsNotPublic(createNewPrivatePost);
-
-        int postId = createNewPrivatePost.jsonPath().getInt("postId");
-
-        assertions.assertPostIdIsPositive(postId);
-
-        lastPostId = createNewPrivatePost.jsonPath().getInt("postId");
-    }
-
-    public void when_UserDeletesPublicPost_expect_PublicPostIsDeleted() {
-        //FPT1-85 [Login Page] Login with valid username and valid password
-        Response signInResponse = apiMethods.signInUser(MR_BEAST_USERNAME, MR_BEAST_PASSWORD);
-
-        assertions.assertStatusCode302(signInResponse.statusCode());
-
-        //FPT1-55 [Delete Post] Delete an Existing Public Post
-        Response deletePublicPost = apiMethods.deletePost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD, lastPostId);
-
-        assertions.assertStatusCode200(deletePublicPost.statusCode());
-        assertions.assertResponseBodyIsEmpty(deletePublicPost);
-    }
-
-    public void when_UserDeletesPrivatePost_expect_PrivatePostIsDeleted() {
-        //FPT1-85 [Login Page] Login with valid username and valid password
-        Response signInResponse = apiMethods.signInUser(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD);
-
-        assertions.assertStatusCode302(signInResponse.statusCode());
-
-        //FPT1-56 [Delete Post] Delete an Existing Private Post
-        Response deletePrivatePost = apiMethods.deletePost(GEORGE_BUSH_USERNAME, GEORGE_BUSH_PASSWORD, lastPostId);
-
-        assertions.assertStatusCode200(deletePrivatePost.statusCode());
-        assertions.assertResponseBodyIsEmpty(deletePrivatePost);
-    }
 }
