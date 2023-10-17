a3daea9697f13d6b36b19218ea934517899aec89
Doychin <doych_trendo@yahoo.com>
2023-10-17 12:09:22 +0300

    Commit changes to CommentInvalidCreationTest to make it completely independent

--- finalprojecttwomenandawoman/src/test/java/restassured/comments/CommentInvalidCreationTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/comments/CommentInvalidCreationTest.java
@@ -1,38 +1,35 @@
 package restassured.comments;
 
 import io.restassured.response.Response;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import restassured.base.BaseCommentTestSetupBeforeAfter;
 
-import static com.telerikacademy.testframework.api.utils.Constants.TOM_CRUISE_PASSWORD;
-import static com.telerikacademy.testframework.api.utils.Constants.TOM_CRUISE_USERNAME;
+import static com.telerikacademy.testframework.api.utils.Constants.*;
 
 public class CommentInvalidCreationTest extends BaseCommentTestSetupBeforeAfter {
-
-    @BeforeEach
-    public void setup() {
-        when_UserCreatesValidPublicPost_expect_ValidPublicPostIsCreated();
-    }
-
-    @AfterEach
-    public void teardown() {
-        when_UserDeletesPublicPost_expect_PublicPostIsDeleted();
-    }
 
     @Test
     //FPT1-170 [Comment] Create Comment With 1001 Characters as Registered User
     public void when_UserCreatesInvalidComment_expect_ErrorStatusCode() {
+        Response createNewPublicPost = apiMethods.createPublicPost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD,
+                POST_DESCRIPTION_VALID);
+        assertions.assertStatusCode200(createNewPublicPost.statusCode());
+        assertions.assertPostContent("Valid Post");
+        assertions.assertPostIsPublic(createNewPublicPost);
+        int postId = createNewPublicPost.jsonPath().getInt("postId");
+        assertions.assertPostIdIsPositive(postId);
+        lastPostId = createNewPublicPost.jsonPath().getInt("postId");
+
         String invalidComment = apiMethods.generateInvalidComment();
-
         Response signInWithUserTomCruise = apiMethods.signInUser(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD);
-
         assertions.assertStatusCode302(signInWithUserTomCruise.statusCode());
 
         Response createCommentResponse = apiMethods.createComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD,
                 invalidComment, lastPostId);
-
         assertions.assertStatusCode400(createCommentResponse.statusCode());
+
+        Response deletePublicPost = apiMethods.deletePost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD, lastPostId);
+        assertions.assertStatusCode200(deletePublicPost.statusCode());
+        assertions.assertResponseBodyIsEmpty(deletePublicPost);
     }
 }
