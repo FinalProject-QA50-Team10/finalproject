d86489572c04b23e7b792db80cff67cd45bd265e
Doychin <doych_trendo@yahoo.com>
2023-10-17 12:07:04 +0300

    Commit changes to CommentLikeTest to make them completely independent

--- finalprojecttwomenandawoman/src/test/java/restassured/comments/CommentLikeTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/comments/CommentLikeTest.java
@@ -1,46 +1,69 @@
 package restassured.comments;
 
 import io.restassured.response.Response;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import restassured.base.BaseCommentTestSetupBeforeAfter;
 
-import static com.telerikacademy.testframework.api.utils.Constants.JACK_NICHOLSON_PASSWORD;
-import static com.telerikacademy.testframework.api.utils.Constants.JACK_NICHOLSON_USERNAME;
+import static com.telerikacademy.testframework.api.utils.Constants.*;
 
 public class CommentLikeTest extends BaseCommentTestSetupBeforeAfter {
 
-    @BeforeEach
-    public void setup() {
-        when_UserCreatesValidPublicPost_expect_ValidPublicPostIsCreated();
-        createCommentAsRegisteredUser();
-    }
-
-    @AfterEach
-    public void teardown() {
-        deleteCommentAsRegisteredUser();
-        when_UserDeletesPublicPost_expect_PublicPostIsDeleted();
-    }
-
     @Test
-    public void when_UserLikesAndDislikesComment_expect_CommentIsLikedAndDisliked() {
-        //FPT1-125 [Like] Verify comments Like button
-        Response signInWithUserJackNicholson = apiMethods.signInUser(JACK_NICHOLSON_USERNAME, JACK_NICHOLSON_PASSWORD);
+    //FPT1-125 [Like] Verify comments Like button
+    public void when_UserLikesComment_expect_CommentIsLiked() {
+        Response createNewPublicPost = apiMethods.createPublicPost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD,
+                POST_DESCRIPTION_VALID);
+        assertions.assertStatusCode200(createNewPublicPost.statusCode());
+        assertions.assertPostContent("Valid Post");
+        assertions.assertPostIsPublic(createNewPublicPost);
+        int postId = createNewPublicPost.jsonPath().getInt("postId");
+        assertions.assertPostIdIsPositive(postId);
+        lastPostId = createNewPublicPost.jsonPath().getInt("postId");
 
-        assertions.assertStatusCode302(signInWithUserJackNicholson.statusCode());
+        Response createCommentResponse = apiMethods.createComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD,
+                COMMENT_DESCRIPTION_VALID, lastPostId);
+        assertions.assertStatusCode200(createCommentResponse.statusCode());
+        lastCommentId = createCommentResponse.jsonPath().getInt("commentId");
 
         Response likeCommentResponse = apiMethods.likeComment(JACK_NICHOLSON_USERNAME, JACK_NICHOLSON_PASSWORD,
                 lastCommentId);
-
         assertions.assertStatusCode200(likeCommentResponse.statusCode());
         assertions.assertCommentIsLiked(likeCommentResponse);
 
-        //FPT1-126 [Like] Verify comments Dislike button
+        Response deletePublicPost = apiMethods.deletePost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD, lastPostId);
+        assertions.assertStatusCode200(deletePublicPost.statusCode());
+        assertions.assertResponseBodyIsEmpty(deletePublicPost);
+    }
+
+    @Test
+    //FPT1-126 [Like] Verify comments Dislike button
+    public void when_UserDislikesComment_expect_CommentIsDisliked() {
+        Response createNewPublicPost = apiMethods.createPublicPost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD,
+                POST_DESCRIPTION_VALID);
+        assertions.assertStatusCode200(createNewPublicPost.statusCode());
+        assertions.assertPostContent("Valid Post");
+        assertions.assertPostIsPublic(createNewPublicPost);
+        int postId = createNewPublicPost.jsonPath().getInt("postId");
+        assertions.assertPostIdIsPositive(postId);
+        lastPostId = createNewPublicPost.jsonPath().getInt("postId");
+
+        Response createCommentResponse = apiMethods.createComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD,
+                COMMENT_DESCRIPTION_VALID, lastPostId);
+        assertions.assertStatusCode200(createCommentResponse.statusCode());
+        lastCommentId = createCommentResponse.jsonPath().getInt("commentId");
+
+        Response likeCommentResponse = apiMethods.likeComment(JACK_NICHOLSON_USERNAME, JACK_NICHOLSON_PASSWORD,
+                lastCommentId);
+        assertions.assertStatusCode200(likeCommentResponse.statusCode());
+        assertions.assertCommentIsLiked(likeCommentResponse);
+
         Response dislikeCommentResponse = apiMethods.dislikeComment(JACK_NICHOLSON_USERNAME, JACK_NICHOLSON_PASSWORD,
                 lastCommentId);
-
         assertions.assertStatusCode200(dislikeCommentResponse.statusCode());
         assertions.assertCommentIsDisliked(dislikeCommentResponse);
+
+        Response deletePublicPost = apiMethods.deletePost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD, lastPostId);
+        assertions.assertStatusCode200(deletePublicPost.statusCode());
+        assertions.assertResponseBodyIsEmpty(deletePublicPost);
     }
 }
