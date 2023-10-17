2d2980c8b6772ad160c02651df49f92f0159dc93
Doychin <doych_trendo@yahoo.com>
2023-10-17 12:12:29 +0300

    Commit changes to AdminCommentEditDeleteTest to make it completely independent

--- finalprojecttwomenandawoman/src/test/java/restassured/comments/AdminCommentEditDeleteTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/comments/AdminCommentEditDeleteTest.java
@@ -1,8 +1,6 @@
 package restassured.comments;
 
 import io.restassured.response.Response;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import restassured.base.BaseCommentTestSetupBeforeAfter;
 
@@ -10,30 +8,54 @@ import static com.telerikacademy.testframework.api.utils.Constants.*;
 
 public class AdminCommentEditDeleteTest extends BaseCommentTestSetupBeforeAfter {
 
-    @BeforeEach
-    public void setup() {
-        when_UserCreatesValidPublicPost_expect_ValidPublicPostIsCreated();
-        createCommentAsRegisteredUser();
-    }
-
-    @AfterEach
-    public void teardown() {
-        when_UserDeletesPublicPost_expect_PublicPostIsDeleted();
-    }
-
     @Test
     //FPT1-62 [Edit Comment] Edit an Existing Comment as Admin
     public void when_AdminEditsComment_expect_CommentIsEdited() {
+        Response createNewPublicPost = apiMethods.createPublicPost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD,
+                POST_DESCRIPTION_VALID);
+        assertions.assertStatusCode200(createNewPublicPost.statusCode());
+        assertions.assertPostContent("Valid Post");
+        assertions.assertPostIsPublic(createNewPublicPost);
+        int postId = createNewPublicPost.jsonPath().getInt("postId");
+        assertions.assertPostIdIsPositive(postId);
+        lastPostId = createNewPublicPost.jsonPath().getInt("postId");
+
+        Response createCommentResponse = apiMethods.createComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD,
+                COMMENT_DESCRIPTION_VALID, lastPostId);
+        assertions.assertStatusCode200(createCommentResponse.statusCode());
+        lastCommentId = createCommentResponse.jsonPath().getInt("commentId");
+
         Response editCommentResponse = apiMethods.editComment(ADMIN_NAME, ADMIN_PASSWORD, EDIT_COMMENT_CONTENT, lastCommentId);
         assertions.assertStatusCode200(editCommentResponse.statusCode());
+
+        Response deletePublicPost = apiMethods.deletePost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD, lastPostId);
+        assertions.assertStatusCode200(deletePublicPost.statusCode());
+        assertions.assertResponseBodyIsEmpty(deletePublicPost);
     }
 
     @Test
     //FPT1-63 [Delete Comment] Delete an Existing Comment as Admin
     public void when_AdminDeletesComment_expect_CommentIsDeleted() {
-        Response deleteCommentResponse = apiMethods.deleteComment(ADMIN_NAME, ADMIN_PASSWORD, lastCommentId);
+        Response createNewPublicPost = apiMethods.createPublicPost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD,
+                POST_DESCRIPTION_VALID);
+        assertions.assertStatusCode200(createNewPublicPost.statusCode());
+        assertions.assertPostContent("Valid Post");
+        assertions.assertPostIsPublic(createNewPublicPost);
+        int postId = createNewPublicPost.jsonPath().getInt("postId");
+        assertions.assertPostIdIsPositive(postId);
+        lastPostId = createNewPublicPost.jsonPath().getInt("postId");
 
+        Response createCommentResponse = apiMethods.createComment(TOM_CRUISE_USERNAME, TOM_CRUISE_PASSWORD,
+                COMMENT_DESCRIPTION_VALID, lastPostId);
+        assertions.assertStatusCode200(createCommentResponse.statusCode());
+        lastCommentId = createCommentResponse.jsonPath().getInt("commentId");
+
+        Response deleteCommentResponse = apiMethods.deleteComment(ADMIN_NAME, ADMIN_PASSWORD, lastCommentId);
         assertions.assertStatusCode200(deleteCommentResponse.statusCode());
         assertions.assertResponseBodyIsEmpty(deleteCommentResponse);
+
+        Response deletePublicPost = apiMethods.deletePost(MR_BEAST_USERNAME, MR_BEAST_PASSWORD, lastPostId);
+        assertions.assertStatusCode200(deletePublicPost.statusCode());
+        assertions.assertResponseBodyIsEmpty(deletePublicPost);
     }
 }
