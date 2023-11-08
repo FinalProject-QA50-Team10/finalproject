44ee82db11856360502e9d916dc52f71c713de6d
Yordan Nikolov <x-normalize@protonmail.com>
2023-11-08 17:35:20 +0200

    Remove unnecessary imports in PrivatePostPostTest

--- finalprojecttwomenandawoman/src/test/java/restassured/posts/PrivatePostPostTest.java
+++ finalprojecttwomenandawoman/src/test/java/restassured/posts/PrivatePostPostTest.java
@@ -2,8 +2,6 @@ package restassured.posts;
 
 import com.telerikacademy.testframework.api.models.ErrorModel;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import restassured.base.BasePostTestSetupBeforeAfter;
 
